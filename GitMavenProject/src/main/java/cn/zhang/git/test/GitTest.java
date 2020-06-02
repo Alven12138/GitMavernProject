63ff6121f003e08284c62b3fcd64c0dc0c10ae21
zhang <zhangyida4748@foxmail.com>
2020-06-03 04:11:56 +0800

    主分支添加

--- GitMavenProject/src/main/java/cn/zhang/git/test/GitTest.java
+++ GitMavenProject/src/main/java/cn/zhang/git/test/GitTest.java
@@ -3,6 +3,7 @@ package cn.zhang.git.test;
 public class GitTest {
     public void test(){
         System.out.println("这是一个测试方法");
+        //主分支添加
         System.out.println("更新1");
         //添加一个注释
         //dev分支添加
