3bb863c8918e412851d615720c3a0932f20ccdfa
zhang <zhangyida4748@foxmail.com>
2020-06-03 04:10:52 +0800

    dev添加了行注释

--- GitMavenProject/src/main/java/cn/zhang/git/test/GitTest.java
+++ GitMavenProject/src/main/java/cn/zhang/git/test/GitTest.java
@@ -2,9 +2,11 @@ package cn.zhang.git.test;
 
 public class GitTest {
     public void test(){
+        //主分支添加
         System.out.println("这是一个测试方法");
         System.out.println("更新1");
         //添加一个注释
         //dev分支添加
+        //dev添加
     }
 }
